c9dd3a85c75c130e6e8208df15191d5fe868da9a
bondaa <legand.bond@gmail.com>
2017-09-20 10:24:04 -0400

    8.2

---
+++ EX8/8.2.py
@@ -0,0 +1,26 @@
+from pprint import pprint
+
+###########
+
+def parse_cdp_neighbors(st):
+    device = st[:3]
+    result = {}
+    c = 0
+    st = [i.strip() for i in st.split('\n')]
+    for line in st:
+        if c == 1:
+            line = [i for i in line.split('  ')]
+            print(line)
+            for i in line:
+                if i == '': 
+                    line.remove('')
+            print(line)
+            continue
+        if line.startswith('Device'):
+            c = 1
+    #print(st)
+
+
+f = open('sw1_sh_cdp_neighbors.txt', 'r')
+st = ' '.join([i for i in f.readlines()])
+pprint(parse_cdp_neighbors(st))

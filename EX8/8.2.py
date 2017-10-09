53533f067b51e72c96714532e5697093028f6c83
bondaraa <legand.bond@gmail.com>
2017-10-09 20:46:48 +0300

    8.3

--- EX8/8.2.py
+++ EX8/8.2.py
@@ -3,34 +3,26 @@ from pprint import pprint
 ###########
 
 def parse_cdp_neighbors(st):
-    device = st[:3]
+    device = [st[:st.find('>')]]
     result = {}
     c = 0
     a=[]
+    count = 0
     st = [i.strip() for i in st.split('\n')]
     for line in st:
         if c == 1:
-            #print(line)
             for i in line.split('  '):
                 if i != '':
                     a.append(i.strip())
                     count += 1
                 if count == 6:
-                    result[device+]
-            
-            print(a)
-                
-            '''
-            line = [i for i in line.split('  ')]
-            print(line)
-            for i in line:
-                if i == '': 
-                    line.remove('')
-            '''
+                    result[device[0] + ' ' + a[1]] = [a[0],a[5]]
+                    count = 0
+                    a.clear()
             continue
         if line.startswith('Device'):
             c = 1
-    #print(st)
+    return(result)
 
 
 f = open('sw1_sh_cdp_neighbors.txt', 'r')

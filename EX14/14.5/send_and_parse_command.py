91d337af76c09b14b12304215f5b62c66da546f2
bondaa <legand.bond@gmail.com>
2017-10-12 03:24:50 -0400

    EX14

--- EX14/14.5/send_and_parse_command.py
+++ EX14/14.5/send_and_parse_command.py
@@ -6,8 +6,8 @@ from netmiko import *
 
 with open('devices.yml') as f:
     templates = yaml.load(f)
-COMMAND = "sh ip int br"
-dictn = {'Command': 'show ip int br', 'Vendor': 'Cisco'}
+COMMAND = "sh clock"
+dictn = {'Command': COMMAND, 'Vendor': 'Cisco'}
 info = send_show_command(templates['routers'],COMMAND)
 for i in info:
     for j in i:
